bb7645de2c351336de90be02b97adf2270d10f83
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-09-13 00:45:28 +0700

    Add KRM specification
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- kpto_codegen/lib/mix/tasks/kpt.gen.ex
+++ kpto_codegen/lib/mix/tasks/kpt.gen.ex
@@ -7,24 +7,25 @@ defmodule Mix.Tasks.Kpt.Codegen do
   def run(args) do
     {opts, [], []} =
       OptionParser.parse(args,
-        strict: [input: :string, outdir: :string],
-        aliases: [i: :input, o: :outdir]
+        strict: [input: :string, outdir: :string, namespace: :string],
+        aliases: [i: :input, o: :outdir, n: :namespace]
       )
 
     input = Keyword.fetch!(opts, :input)
     outdir = Keyword.fetch!(opts, :outdir)
+    namespace = Keyword.fetch!(opts, :namespace)
 
     input
     |> Path.expand()
     |> YamlElixir.read_from_file!()
-    |> KptoCodegen.generate()
+    |> KptoCodegen.generate(namespace)
     |> Enum.map(&format_and_write(&1, outdir))
   end
 
-  defp format_and_write({filename, code}, outdir) do
-    File.mkdir_p!(outdir)
+  defp format_and_write({dir, filename, code}, outdir) do
+    File.mkdir_p!(Path.join(outdir, dir))
 
-    Path.join([outdir, filename])
+    Path.join([outdir, dir, filename])
     |> File.write!(Code.format_string!(code))
   end
 end
